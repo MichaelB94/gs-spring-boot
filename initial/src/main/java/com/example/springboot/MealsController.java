8c3afa037dc848ec074cc36ba2d4e2e210c60eae
MichaelB94 <124793101+MichaelB94@users.noreply.github.com>
2023-06-05 12:08:08 +0200

    ex2.01

---
+++ initial/src/main/java/com/example/springboot/MealsController.java
@@ -0,0 +1,25 @@
+package com.example.springboot;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MealsController {
+    public List<Meal> meals = Arrays.asList(
+            new Meal("Pasta Pesto","Deliziose linguine al pesto di basilico",12.99),
+            new Meal("Pizza","Semplicemente Margherita: con pomodoro fresco e fiordilatte",10),
+            new Meal("Tagliere misto","Selezione dei migliori salumi italiani",7.50)
+            );
+
+    @GetMapping("/meals")
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+}
